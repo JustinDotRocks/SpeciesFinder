076e220ab31efdb3d8d5325138dffe4acf427828
JustinDotRocks <justins192107@gmail.com>
2024-01-18 18:59:32 -0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
+  
+}
+
