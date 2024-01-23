beb1c5906c0e887be941737c17c0129f06d53175
JustinDotRocks <justins192107@gmail.com>
2024-01-22 20:57:32 -0330

    Made the main species selection menu full screen for mobile. Fixed some issues with my TailwindCSS configuration as well.

---
+++ src/index.js
@@ -0,0 +1,30 @@
+// fetch('data.json')
+//   .then(response => response.json())
+//   .then(data => {
+//     // Use your data here
+//     console.log(data);
+//   })
+//   .catch(error => console.error('Error:', error));
+
+//   document.addEventListener('DOMContentLoaded', () => {
+//     // Selecting the button using its class
+//     const menuButton = document.querySelector('.menu-button');
+    
+//     menuButton.addEventListener('click', () => {
+//       // Assuming you have another element to toggle, like a menu
+//       const menu = document.querySelector('.menu');
+//       menu.classList.toggle('hidden');
+//     });
+//   });
+  
+
+// Toggling the menu on mobile
+document.getElementById('menuToggle').addEventListener('click', function() {
+    document.querySelector('aside').classList.toggle('-translate-x-full');
+});
+  
+
+// Adding click to triangle button
+document.getElementById('arrow-button').addEventListener('click', () => {
+    console.log('Triangle button clicked!');
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
-  
+  plugins: [],  
 }
 
