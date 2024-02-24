5d7e3ae66ec89a4d602daf54e0553ba666350cb9
JustinDotRocks <justins192107@gmail.com>
2024-02-24 10:19:40 -0330

    Updating the way the data is fetched. Only fetching the images we need for the current categoy. Currently isn't reaching out to the API properly.

--- src/index.js
+++ src/index.js
@@ -126,10 +126,9 @@
             const data = await response.json();
             const container = document.querySelector('#species-selection');
             container.innerHTML = ''; // Clear existing content
-
+    
             Object.entries(data.species).forEach(([category, speciesList]) => {
-                // Correctly replace spaces for ID and data-target attributes
-                const formattedCategoryName = category.replace(/\s+/g, '-');
+                const formattedCategoryName = category.replace(/\s+/g, '-'); // Replaces the whitspace with "-".
                 let categoryHtml = `
                     <div class="flex justify-between items-center bg-white text-customBlue p-4 rounded-t-md">
                         <h2 class="text-xl font-bold">${category.charAt(0).toUpperCase() + category.slice(1)}</h2>
@@ -139,39 +138,63 @@
                     </div>
                     <div id="${formattedCategoryName}Cards" class="hidden flex flex-col items-center">
                 `;
+    
                 let cardsHtml = speciesList.map(species => {
-                    const truncatedDescription = species.description.length > 75 ? species.description.substring(0, 75) + '...' : species.description;
-                        return `
-                            <div class="card max-w-md bg-white rounded-lg border border-gray-200 shadow-md m-8 p-4 cursor-pointer" role="button" onclick="openModal('${species.common_name}', '${species.image}', '${species.description}')">
-                                <h3 class="text-xl text-customBlue font-semibold">${species.common_name}</h3>
-                                <div class="flex -mx-4">
-                                    <img src="${species.image}" alt="${species.common_name}" data-taxon-id="${species.taxon_id}" class="species-image w-3/4 h-3/4 rounded-md p-4">
-                                    <p class="text-gray-700 mt-2 px-4 w-1/2">${truncatedDescription}</p>
-                                </div>
+                    const truncatedDescription = species.description.length > 75 ? `${species.description.substring(0, 75)}...` : species.description;
+                    return `
+                        <div class="card max-w-md bg-white rounded-lg border border-gray-200 shadow-md m-8 p-4 cursor-pointer" role="button" onclick="openModal('${species.common_name}', '', '${species.description}')">
+                            <h3 class="text-xl text-customBlue font-semibold">${species.common_name}</h3>
+                            <div class="flex -mx-4">
+                                <img src="" alt="${species.common_name}" data-taxon-id="${species.taxon_id}" class="species-image w-3/4 h-3/4 rounded-md p-4">
+                                <p class="text-gray-700 mt-2 px-4 w-1/2">${truncatedDescription}</p>
                             </div>
-                        `;
-                        }).join('');
-                        // Append the category and its cards to the container
-                        container.innerHTML += categoryHtml + cardsHtml + '</div>';
-                    });
+                        </div>
+                    `;
+                }).join('');
+                
+                // Append the category and its cards to the container
+                container.innerHTML += categoryHtml + cardsHtml + '</div>';
+            });
             attachToggleEventListeners();
-            // Asynchronously update images without waiting for all to complete before rendering categories.
-            updateSpeciesImages();
         } catch (error) {
             console.error('Error loading the JSON data:', error);
         }
     };
 
-    const attachToggleEventListeners = () => {
+    const updateButtonIcon = (button, isVisible) => {
+        button.innerHTML = isVisible ?
+            `<svg class="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>` : // Icon for "collapse"
+            `<svg class="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7"></path></svg>`; // Icon for "expand"
+    }
+    
+
+    const attachToggleEventListeners = (data) => {
         document.querySelectorAll('.toggle-category').forEach(button => {
-            button.addEventListener('click', () => {
+            button.addEventListener('click', async () => {
                 const targetId = button.getAttribute('data-target');
-                const target = document.getElementById(targetId);
-                target.classList.toggle('hidden');
-                // Toggle the arrow direction
-                button.innerHTML = target.classList.contains('hidden') ? 
-                    `<svg class="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>` : 
-                    `<svg class="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7"></path></svg>`;
+                const targetContainer = document.getElementById(targetId.replace('Cards', '') + 'Cards');
+                const isVisible = targetContainer.classList.toggle('hidden');
+    
+                // Update button icon based on visibility
+                updateButtonIcon(button, isVisible);
+    
+                if (!isVisible) {
+                    return; // If we are hiding the container, no need to fetch images
+                }
+    
+                // Extract categoryName from targetId
+                const categoryName = targetId.replace('Cards', '');
+                if (!data.species[categoryName]) return; // Safety check
+    
+                // Fetch and update images only if they haven't been loaded yet
+                const imagesToUpdate = targetContainer.querySelectorAll('img[src=""]');
+                if (imagesToUpdate.length > 0) {
+                    for (let img of imagesToUpdate) {
+                        const taxonId = img.getAttribute('data-taxon-id');
+                        const imageUrl = await fetchImageFromAPI(taxonId);
+                        if (imageUrl) img.src = imageUrl;
+                    }
+                }
             });
         });
     };
@@ -185,17 +208,17 @@
     };
     
     const fetchImageFromAPI = async (taxonId) => {
-        const url = `https://api.inaturalist.org/v1/observations?taxon_id=${taxonId}&per_page=1?size=large`;
+        const url = `https://api.inaturalist.org/v1/observations?taxon_id=${taxonId}&per_page=1&size=large`;
         try {
             const response = await fetch(url);
             const data = await response.json();
-            console.log(data.results[0]?.photos[0]?.url)
-            return data.results[0]?.photos[0]?.url || ''; // Return a default image URL if not found
+            return data.results[0]?.photos[0]?.url || ''; // Provide a fallback URL or leave empty
         } catch (error) {
             console.error(`Failed to fetch image for taxon ID ${taxonId}:`, error);
-            return 'default_placeholder_image_url'; // Fallback image URL
+            return ''; // Fallback if error
         }
     };
+    
     // loadSpeciesData to call displaySpecies with the entire data object
     const loadSpeciesData = async () => {
         try {
@@ -206,8 +229,7 @@
             console.error('Error loading the JSON file:', error);
         }
     };
-
-
+    
 
     document.addEventListener('DOMContentLoaded', () => {
         document.addEventListener('DOMContentLoaded', loadSpeciesData);
